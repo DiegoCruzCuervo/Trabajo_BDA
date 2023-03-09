9d4aaa6255670fe07f67e99001d11452dc3849a4
Alejandro Cortijo <124038533+Cortijo02@users.noreply.github.com>
2023-03-09 16:54:28 +0100

    S1 Done

--- Sesion_1/Practica 1.sql
+++ Sesion_1/Practica 1.sql
@@ -47,7 +47,7 @@ from tablaunica
 where tipo = "Comida"
 group by nombre_local;
 
--- 1 Falla No claves
+-- 1.4 a) Falla No claves
 -- Los datos de un nuevo local de la cadena de pizzerías con identificador 4, nombre La Cuarta, y situada en la calle Venus y zona 3.
  
 start transaction; 
@@ -56,7 +56,7 @@ start transaction;
 commit;
 rollback;
 
--- 2 Falla Medida
+-- 1.4 b) Falla Medida
 -- Los datos de un nuevo pedido (identificador 344) del cliente zampa con identificador 25, al local "La Primera" (identificador 1) de una Pizza Cuatro quesos el 28 de
 -- febrero de 2023 a las 21:00 por un importe total de 18€ y entrega a domicilio.
 
@@ -66,7 +66,7 @@ start transaction;
 commit;
 rollback;
 
--- 3 OK
+-- 1.4 c) OK
 -- Los datos de un nuevo pedido (identificador 501) a domicilio del cliente new23 (identificador 35) de la calle Marte y zona 2, a la pizzería “La Primera” de 2 Pizza
 -- Vegana de tamaño mediano acogiéndose a la oferta de 2x1 a domicilio, con valor subtotal y total de 13€. 
 
@@ -76,7 +76,7 @@ start transaction;
 commit;
 rollback;
 
--- 4
+-- 1.4 d) OK
 -- Los datos de un nuevo pedido (identificador 223) de recogida en local del cliente maria1 (identificador 3), a la pizzería “La Primera” de la calle Luna (zona 3), de 1
 -- Pizza Carbonara familiar por un precio de 18€ (sin descuentos) con fecha de “14/02/2023 21:45:00”.
 
@@ -84,16 +84,14 @@ start transaction;
 	insert into tablaunica (pedidoId, usuario, clienteId, nombre_local, calle_cliente, zona_cliente, cantidad, producto, medida, precio, fecha_hora)
 	values (223, "maria1", 3, "La Primera", "Luna", 3, 1, "Pizza Carbonara", "Familiar", 18, "2023-02-14 21:45:00");
 commit;
-rollback; 
+rollback;
 
+-- 1.5 a) Cambiar la zona de la calle Sol a valor 4.
 
+update tablaunica
+set zona_cliente = 4
+where calle_cliente = "Sol";
 
+-- 1.5 b) Debido a un error, se debe cancelar el pedido 170, eliminando los datos de dicho pedido. 
 
-
-
-
-
-
-
-
-
+delete from tablaunica where pedidoID = 170;
