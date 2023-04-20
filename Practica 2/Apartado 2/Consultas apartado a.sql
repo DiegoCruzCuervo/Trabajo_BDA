7da897300f6b6fbe51824b3d414679331d29c6e7
Diego Cruz Cuervo <diego.cruz@alumnos.upm.es>
2023-04-20 17:23:11 +0200

    Comienzo de rehacer la practica
    He comenzado a rehacer la practica.

---
+++ Practica 2/Apartado 2/Consultas apartado a.sql
@@ -0,0 +1,12 @@
+-- Apartado 1 (Inner Join)
+select distinct nombre_producto
+from productos p inner join detallepedidos d on p.productoID = d.productoID
+where d.cantidad > 5;
+
+-- Variante (Subconsulta)
+select distinct nombre_producto
+from productos p
+where exists ( 	select productoID
+				from detallepedidos d
+                where p.productoID = d.productoID and d.cantidad > 5
+			);
