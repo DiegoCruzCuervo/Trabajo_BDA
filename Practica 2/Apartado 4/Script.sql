fb2507a2a3ccd68b297413e0e69c5ba20378f1ce
“DiegoCruzCuervo” <diego.cruz@alumnos.upm.es>
2023-04-20 19:42:57 +0200

    Ejercicio 4 con error

---
+++ Practica 2/Apartado 4/Script.sql
@@ -0,0 +1,53 @@
+use practica2bda22_23;
+-- Ejercicio 5
+-- Update b
+
+-- Pedidos de 2023
+select pedidoID
+from pedidos
+where year(fecha_venta)=2023;
+
+-- Productos vendidos con categoria Bicicleta
+select ProductoId
+from productos p inner join subcategoriaproducto sb on p.subcategoriaID=sb.subcategoriaID
+inner join categoriaproducto cp on sb.categoriaID=cp.categoriaID
+where cp.nombre_categoria='Bicicleta'
+group by productoId;
+
+-- Productos que ya tenian descuento
+select productoID
+from detallepedidos
+where descuento_unitario > 0
+group by productoID;
+
+-- Consulta grande 
+select *
+from detallepedidos
+where  descuento_unitario > 0 and
+pedidoID in (select pedidoID
+from pedidos
+where year(fecha_venta)=2023
+group by PedidoID) and
+ProductoId in (select ProductoId
+from productos p inner join subcategoriaproducto sb on p.subcategoriaID=sb.subcategoriaID
+inner join categoriaproducto cp on sb.categoriaID=cp.categoriaID
+where cp.nombre_categoria='Bicicleta'
+group by ProductoId);
+
+-- Actualizacion
+start transaction;
+update detallepedidos
+set descuento_unitario=descuento_unitario+1
+where pedidoId in (select pedidoID
+from detallepedidos
+where  descuento_unitario > 0 and
+pedidoID in (select pedidoID
+from pedidos
+where year(fecha_venta)=2023
+group by PedidoID) and
+ProductoId in (select ProductoId
+from productos p inner join subcategoriaproducto sb on p.subcategoriaID=sb.subcategoriaID
+inner join categoriaproducto cp on sb.categoriaID=cp.categoriaID
+where cp.nombre_categoria='Bicicleta'
+group by ProductoId));
+rollback;
