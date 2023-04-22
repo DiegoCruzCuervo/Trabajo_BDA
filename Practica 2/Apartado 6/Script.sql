378a3c17b325a053dcb056006200c24c21d8e7d6
“DiegoCruzCuervo” <diego.cruz@alumnos.upm.es>
2023-04-22 13:16:22 +0200

    Consulta apartado 6b

--- Practica 2/Apartado 6/Script.sql
+++ Practica 2/Apartado 6/Script.sql
@@ -1,8 +1,28 @@
 use practica2bda22_23;
 -- Ejercicio 6
--- Consulta
-select pedidoID, primer_nombre, nombre_territorio, fecha_venta, sum((precio_unitario - descuento_unitario) * cantidad) as sumaTotal
-from clientes c inner join territorioventas tv on c.territorioId=tv.territorioId
-inner join pedidos p on p.ClienteId=c.ClienteID
-inner join detallepedidos dp on dp.PedidoId=p.PedidoId
-group by pedidoId;
+
+-- Sacar la suma de los detalles de cada pedido
+select pedidoID, sum((precio_unitario - descuento_unitario)*cantidad) as SumaTotal
+from detallepedidos
+group by pedidoID;
+
+-- Nombre y pais de los clientes
+select clienteId, primer_nombre, nombre_territorio
+from clientes c inner join territorioventas t on c.territorioId=t.territorioId;
+
+-- Fecha del pedido y cliente que lo realizó
+select pedidoId, clienteID, fecha_venta
+from pedidos;
+
+-- Consulta 2+3
+select primer_nombre, nombre_pais, pedidoID, fecha_venta
+from clientes c inner join territorioventas t on c.territorioId=t.territorioId
+inner join pedidos p on c.clienteId=p.clienteId;
+
+-- Consulta final
+select primer_nombre, nombre_pais, fecha_venta, SumaTotal
+from clientes c inner join territorioventas t on c.territorioId=t.territorioId
+inner join pedidos p on c.clienteId=p.clienteId
+inner join (select pedidoID, sum((precio_unitario - descuento_unitario)*cantidad) as SumaTotal
+from detallepedidos
+group by pedidoID) as TablaAux on p.pedidoId=TablaAux.pedidoId;
